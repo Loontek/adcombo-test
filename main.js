086f3c127d79840a92a0b3a492c3f3e306f82ddd
Loontek <sojjkin02@mail.ru>
2023-03-31 01:34:38 +0300

    fix: change accordion logic

--- main.js
+++ main.js
@@ -2,9 +2,14 @@ import './style.css';
 import control from './src/assets/img/control.svg';
 
 const accordion = document.querySelector('.advantages__list');
+const accordionItems = document.querySelectorAll(".advantages__item")
 
 accordion.addEventListener('click', (e)=> {
   if(!e.target.classList.contains('advantage__title')) return;
+
+  for(let item of accordionItems) {
+    item.classList.remove('advantage_active');
+  }
   
   e.target.parentNode.parentNode.classList.toggle('advantage_active');
 })
