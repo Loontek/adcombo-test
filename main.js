e71e16cc13512eb740cf1a3fe6cd674e234fa75d
Loontek <sojjkin02@mail.ru>
2023-03-30 21:20:47 +0300

    feat: add mobile styles

--- main.js
+++ main.js
@@ -1,1 +1,38 @@
-import './style.css'
+import './style.css';
+
+const accordion = document.querySelector('.advantages__list')
+
+console.log(accordion)
+
+accordion.addEventListener('click', (e)=> {
+  if(!e.target.classList.contains('advantage__title')) return;
+
+  if(e.target.parentNode.classList.contains('advantage_active')) {
+    setTimeout(()=>{
+      e.target.parentNode.classList.remove('advantage_active');
+      e.target.parentNode.classList.remove('advantage_closing');
+    },1000)
+
+    e.target.parentNode.classList.add('advantage_closing');
+
+    return
+  }
+
+  e.target.parentNode.classList.add('advantage_active');
+  // e.target.parentNode.classList.`toggle`('advantage_active');
+})
+
+// Slider
+
+$(document).ready(function(){
+  $(".slider__content").bxSlider({
+    minSlides: 1,
+    maxSlides: 1,
+    slideWidth: 176,
+    pagerCustom: '#bx-pager',
+    nextSelector: '#slider-next',
+    prevSelector: '#slider-prev',
+    nextText: `<img src="./src/assets/img/control.svg" alt="next control">`,
+    prevText: `<img src="./src/assets/img/control.svg" alt="prev control">`,
+  });
+});
